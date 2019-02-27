8bac39c7cf0183f57c36ec1cee0393dba2db2597
acn116 <acn116@skbl-422-013.ph.ic.ac.uk>
2019-02-27 12:32:43 +0000

    Commented multiplication.asm and cleaned up main.asm

--- main.asm
+++ main.asm
@@ -14,31 +14,31 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 MAIN_PROG CODE                      ; let linker place main program
 
 START
-	call	ADC_Setup
-	call	DAC_Setup
-	call	LCD_Setup
-	call	Volume_Setup
+	call	ADC_Setup	    ; run ADC Setup
+	call	DAC_Setup	    ; run DAC Setup
+	call	LCD_Setup	    ; run LCD Setup
+	call	Volume_Setup	    ; run Volume Setup
 	goto    LCD_loop
 	
 
 measure_loop
-	call	ADC_read_A0   ;analog to digital conversion
+	call	ADC_read_A0	    ;analog to digital conversion
 	movf	ADRESH, W
-	addlw   b'00110000' ;convert the second 8bits (with configuration bits) to analog
-	call    DAC_write  ;convert the first 8 bits of the digital data to analog
+	addlw   b'00110000'	    ;convert the second 8bits (with configuration bits) to analog
+	call    DAC_write	    ;convert the first 8 bits of the digital data to analog
 	movf	ADRESL, W
 	call    DAC_write
 	call	DAC_end_write
-	goto	measure_loop	; loop forever       
+	goto	measure_loop	    ; loop forever       
 frequency_mix
-	call	ADC_read_A0 ;convert analog to digital
-        movff	ADRESL,k16+1 ;move the lower bits of the digital data to k_16+1
-        movff	ADRESH, k16 ;move the higher bits of the digital data to k_16
-        call	ADC_read_A5 ;convert the other analog signal to digital
-        movff	ADRESL, mul16+1 ;move the lower bits of the other digital data to mul_16+1
-        movff	ADRESH, mul16 ;move the higher bits of the other digital data to mul_16
-	call	frequency_multiplication
-	call	DAC_write ;digital to analog conversion
+	call	ADC_read_A0	    ;convert analog to digital
+        movff	ADRESL,k16+1	    ;move the lower bits of the digital data to k_16+1
+        movff	ADRESH, k16	    ;move the higher bits of the digital data to k_16
+        call	ADC_read_A5	    ;convert the other analog signal to digital
+        movff	ADRESL, mul16+1	    ;move the lower bits of the other digital data to mul_16+1
+        movff	ADRESH, mul16	    ;move the higher bits of the other digital data to mul_16
+	call	frequency_multiplication    ; mix signals
+	call	DAC_write	    ;digital to analog conversion
         movf	output_tmp, W 
         call	DAC_write
         call	DAC_end_write
--- multiplication.asm
+++ multiplication.asm
@@ -3,52 +3,52 @@
     global  k16, k8, mul16, prod8x16, prod16x16
     global  multiplication16x16, multiplication8x16
 
-acs0    udata_acs   ; named variables in access ram
-k16	res 2	    ; reserve 2 bytes for k16
-k8	res 1	    ; reserve 1 byte for k8
-mul16	res 2	    ; reserve 2 bytes for mul16
-prod8x16    res	3   ; reserve 3 bytes for prod8x16
-prod16x16   res	4   ; reserve 4 bytes for prod16x16
-bit	res 1	    ; reserve 1 byte for bit
+acs0	    udata_acs		    ; named variables in access ram
+k16	    res 2		    ; reserve 2 bytes for k16
+k8	    res 1		    ; reserve 1 byte for k8
+mul16	    res 2		    ; reserve 2 bytes for mul16
+prod8x16    res	3		    ; reserve 3 bytes for prod8x16
+prod16x16   res	4		    ; reserve 4 bytes for prod16x16
+bit	    res 1		    ; reserve 1 byte for bit
 
 acs_ovr	access_ovr
-mul1_tmp res 2   ; reserve 2 byte for variable mul1_tmp
-mul2_tmp res 3   ; reserve 3 byte for variable mul2_tmp
+mul1_tmp    res 2		    ; reserve 2 byte for variable mul1_tmp
+mul2_tmp    res 3		    ; reserve 3 byte for variable mul2_tmp
 
 Multiplication code
  
 multiplication8x16
-    mulwf   k16+1
-    movff   PRODH, mul1_tmp
-    movff   PRODL, mul1_tmp+1
-    mulwf   k16
-    movff   mul1_tmp+1, prod8x16+2
-    movf    mul1_tmp, W
-    addwfc  PRODL, 1
-    movff   PRODL, prod8x16+1
-    movlw   0x00
-    addwfc  PRODH, 1
-    movff   PRODH, prod8x16
+    mulwf   k16+1		    ; multiply lower byte with 8-bit number
+    movff   PRODH, mul1_tmp	    ; move upper byte of lower byte multiplication to temporary storage
+    movff   PRODL, mul1_tmp+1	    ; move lower byte of lower byte multiplication to temporary storage
+    mulwf   k16			    ; multiply upper byte with 8-bit number
+    movff   mul1_tmp+1, prod8x16+2  ; move lower byte of lower byte multiplication to lowest result byte
+    movf    mul1_tmp, W		    ; move upper byte of lower byte multiplication to W for adding
+    addwfc  PRODL, 1		    ; add upper byte of lower byte multiplication to lower byte of upper byte multiplication with carry
+    movff   PRODL, prod8x16+1	    ; move previous result to middle byte of result
+    movlw   0x00		    ; move 0x0 to W for adding
+    addwfc  PRODH, 1		    ; add upper byte of upper byte multiplication to 0x0 with carry
+    movff   PRODH, prod8x16	    ; move previous result to upper byte of result
     return
     
 multiplication16x16
-    movf    mul16+1, W
-    call    multiplication8x16
-    movff   prod8x16, mul2_tmp
-    movff   prod8x16+1, mul2_tmp+1
-    movff   prod8x16+2, mul2_tmp+2
-    movf    mul16, W
-    call    multiplication8x16
-    movff   mul2_tmp+2, prod16x16+3
-    movff   mul2_tmp+1, prod16x16+2
-    movf    prod8x16+2, W
-    addwfc  prod16x16+2, 1
-    movff   mul2_tmp, prod16x16+1
-    movf    prod8x16+1, W
-    addwfc  prod16x16+1, 1
-    movff   prod8x16, prod16x16
-    movlw   0x00
-    addwfc  prod16x16
+    movf    mul16+1, W		    ; move lower byte of factor to W for multiplication
+    call    multiplication8x16	    ; multiply lower byte of factor to other factor in k16 and k16+1
+    movff   prod8x16, mul2_tmp	    ; move highest byte of multiplication to temporary storage
+    movff   prod8x16+1, mul2_tmp+1  ; move middle byte of multiplication to temporary storage
+    movff   prod8x16+2, mul2_tmp+2  ; move lowest byte of multiplication to temporary storage
+    movf    mul16, W		    ; move high byte of factor to W for multiplication
+    call    multiplication8x16	    ; multiply high byte of factor to other factor in k16 and k16+1
+    movff   mul2_tmp+2, prod16x16+3 ; move lowest byte of 1st multiplication to lowest byte of result
+    movff   mul2_tmp+1, prod16x16+2 ; move middle byte of 1st multiplication to middle low byte of result for adding
+    movf    prod8x16+2, W	    ; move lowest byte of 2nd multiplication to W for adding
+    addwfc  prod16x16+2, 1	    ; add middle byte of 1st multiplication to lowest byte of 2nd multiplication with carry and save in middle low byte of result
+    movff   mul2_tmp, prod16x16+1   ; move high byte of 1st multiplication to middle high byte of result for adding
+    movf    prod8x16+1, W	    ; move middle byte of 2nd multiplication to W for adding
+    addwfc  prod16x16+1, 1	    ; add high byte of 1st multiplication to middle byte in 2nd multiplication with carry and save in middle high byte of result
+    movff   prod8x16, prod16x16	    ; move high byte of 2nd multiplication to highest byte of result for adding
+    movlw   0x00		    ; move 0x0 to W for adding
+    addwfc  prod16x16		    ; add 0x0 to high byte of 2nd multiplication with carry and save as highest byte of result
     return
  
 
